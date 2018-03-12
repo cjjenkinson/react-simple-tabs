7782f8ac3e02ad3054c91a33acaabf215acf8777
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-12 10:52:35 +0100

    Chore: added Jest config

---
+++ __tests__/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['**/src/*.js'],
+  coverageDirectory: './__tests__/__coverage__',
+  coveragePathIgnorePatterns: ['dist/', 'node_modules/', '__mocks__/'],
+  coverageReporters: ['text-summary', 'text', 'json', 'lcov'],
+  rootDir: '..',
+};
