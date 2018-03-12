453abfec5d5c3b14ce70505cec8355f2b0f6b3d5
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-12 15:03:54 +0100

    Test: add boilerplate

---
+++ __tests__/SimpleTabs.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactPopup from '../src/react-popup';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { shallow } from 'enzyme';
+import { stub } from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('React Simple Tabs DOM', () => {
+
+});
